5e1e58c37081908f517bc36acbefa1750b73bbdd
bluetoothless <sersaw@onet.pl>
2022-04-26 22:31:07 +0200

    Adding interface in Unity
    A part of the user interface.

---
+++ unity/CTF/Assets/Scripts/SliderValueScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueScript : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI sliderText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.onValueChanged.AddListener((v) => {
+            sliderText.text = v.ToString("0");
+        });
+    }
+}
