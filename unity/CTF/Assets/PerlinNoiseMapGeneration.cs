b57284b9afe03987b7b5a5a21ff1cfaa0234547e
eragoh <kacpergrau@gmail.com>
2022-05-01 13:40:02 +0200

    Testing speed with old wrong algorithm
    for 20% for each biom => 0,1s
    for 70% for each biom => 0,7s

--- unity/CTF/Assets/PerlinNoiseMapGeneration.cs
+++ unity/CTF/Assets/PerlinNoiseMapGeneration.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Biom
@@ -27,6 +28,11 @@ public class PerlinNoiseMapGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Stopwatch sw = new Stopwatch();
+
+        sw.Start();
+
+
         GameObject field = gameObject;
         int TileScale = 4;
 
@@ -38,11 +44,12 @@ public class PerlinNoiseMapGeneration : MonoBehaviour
         int height = (int)field.transform.localScale.x * 10; // 360
 
         // hardcoded map %
-        Biom lakes = new(60, lakePrefab, lakeTiles);
-        Biom accelerate = new(60, acceleratePrefab, accelerateTiles);
-        Biom desert = new(60, desertPrefab, desertTiles);
+        Biom lakes = new(70, lakePrefab, lakeTiles);
+        Biom accelerate = new(70, acceleratePrefab, accelerateTiles);
+        Biom desert = new(70, desertPrefab, desertTiles);
 
         var bioms = new List<Biom> { accelerate, desert, lakes };
+        bool[,] mapMatrix = new bool[360, 400];
 
         float scale;
         float offsetX;
@@ -53,6 +60,9 @@ public class PerlinNoiseMapGeneration : MonoBehaviour
             scale = Random.Range(2f, 3f);
             offsetX = Random.Range(0f, 99999f);
             offsetY = Random.Range(0f, 99999f);
+
+            int i = 0;  // height counter
+            int j = 0;  // width counter
 
             for (int current_tile_height = (height / 2 - height) + (TileScale / 2); current_tile_height < height / 2; current_tile_height += TileScale)
             {
@@ -69,9 +79,16 @@ public class PerlinNoiseMapGeneration : MonoBehaviour
                         GameObject newTile = Instantiate(biom.prefab, new Vector3(current_tile_height, 0, current_tile_width), Quaternion.identity);
                         newTile.transform.SetParent(biom.tiles.transform);
                     }
+                    j++;
                 }
+                i++;
             }
         }
+
+        sw.Stop();
+
+        UnityEngine.Debug.Log("Elapsed= " + sw.ElapsedTicks + "/// Seconds = " + sw.Elapsed);
+
         /*
         for (int current_tile_height = (height/2 - height) + (TileScale/2); current_tile_height < height/2; current_tile_height += TileScale)
         {
