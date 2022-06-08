8900e37ba7ebfa40a3a3beee338d42cb4fc22b4a
eragoh <kacpergrau@gmail.com>
2022-06-08 20:29:40 +0200

    letter case bugfix

--- unity/CTF/Assets/Scripts/deadlyWaterScript.cs
+++ unity/CTF/Assets/Scripts/deadlyWaterScript.cs
@@ -26,16 +26,4 @@ public class DeadlyWaterScript : MonoBehaviour
             agent.GetComponent<AgentMovementWSAD>().Kill();
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
