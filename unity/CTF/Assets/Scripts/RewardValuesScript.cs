8bbe9ce6e7bb505c74b10a7af11dfeb1136c6b36
bluetoothless <sersaw@onet.pl>
2022-07-27 20:53:17 +0200

    Added Rewards.json
    Reward values are now easily changeble from the file CTF/Assets/Rewards.json. New reward types can be added to RewardValuesScript.

---
+++ unity/CTF/Assets/Scripts/RewardValuesScript.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class RewardValuesScript
+{
+    public float gameWon;
+    public float flagCaptured;
+    public float flagRetrievedFromAgent;
+    public float gameLost;
+    public float agentDead;
+
+    public void saveRewardValues()
+    {
+        var jsonRewards = JsonUtility.ToJson(this);
+        var path = Application.dataPath + "/Rewards.json";
+        File.WriteAllText(path, jsonRewards);
+    }
+
+    public RewardValuesScript getRewardValues()
+    {
+        var jsonRewards = File.ReadAllText(Application.dataPath + "/Rewards.json");
+        var rewardValues = JsonUtility.FromJson<RewardValuesScript>(jsonRewards);
+        return rewardValues;
+    }
+}
+
+/*  READING VALUES FROM JSON
+        var rewardValues = new RewardValuesScript().getRewardValues();
+        var exampleNeededValue = rewardValues.gameLost;
+*/
+
+/*  SAVING VALUES TO JSON
+        var rewardValues = new RewardValuesScript
+        {
+            gameWon = 1.0F,
+            flagCaptured = 0.2F,
+            flagRetrievedFromAgent = 0.2F,
+            gameLost = 1.0F,
+            agentDead = 0.9F
+        };
+        rewardValues.saveRewardValues();
+*/
