31997ef82a466415e978270d1f9aa435545d99c6
Chlodwig-tech <80710710+Chlodwig-tech@users.noreply.github.com>
2022-11-22 21:17:54 +0100

    Update TestLevel3.cs
    Adding level 3 - agents go circles

--- unity/CTF/Assets/Scripts/TrainingScenarios/TestLevel3.cs
+++ unity/CTF/Assets/Scripts/TrainingScenarios/TestLevel3.cs
@@ -6,10 +6,33 @@ public class TestLevel3 : TestLevel
 {
     public override (bool, bool, bool, bool) Run()
     {
-        bool W = false;
+        bool W = true;
         bool S = false;
-        bool A = false;
+        bool A = Random.Range(0,100) > 33;
         bool D = false;
         return (W, S, A, D);
+    }
+
+    public override void Spawn()
+    {
+        foreach (var agent in GameManager.BlueAgents)
+        {
+            agent.transform.position = new Vector3(agent.transform.position.x, 20, agent.transform.position.z);
+        }
+
+        var blueBaseScript = GameObject.Find("Blue Base(Clone)").GetComponent<BlueBaseScript>();
+
+        for (int i = 0; i < Random.Range(4, 10); i++)
+        {
+            int index;
+            do
+            {
+                index = Random.Range(0,8899);
+                if (blueBaseScript.CheckIfCanSpawnAt(index))
+                    break;
+            } while (true);
+
+            blueBaseScript.SpawnAgentAt(index);
+        }
     }
 }
